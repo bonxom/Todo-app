3c828caccf3c353f7fdaf66471b8c2ad842a156d
bonxom <bonxom@gmail.com>
2026-08-07 17:19:13 +0700

    config: deploy

--- backend/config/env.ts
+++ backend/config/env.ts
@@ -7,6 +7,8 @@ const DEFAULT_ALLOWED_ORIGINS: string[] = [
   'http://127.0.0.1:5000',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
+  'http://localhost:3636',
+  'http://127.0.0.1:3636',
   'http://180.93.34.142:3636',
   'https://todo.onrender.com',
   'http://todo.onrender.com'
